db0423c4113edd79daf90f4d14e3366c46f08f17
Sai Sahith V K <vksaisahith272@gmail.com>
2021-10-03 14:20:20 +0000

    com

--- ssvk_project/target/run/my_new_project/models/final/emp_details.sql
+++ ssvk_project/target/run/my_new_project/models/final/emp_details.sql
@@ -3,6 +3,6 @@
       create or replace transient table testdb.dbt_ssvk.emp_details  as
       (
 
-select * from snowflake_sample_data.tpch_sf001.region
+select * from snowflake_sample_data.tpch_sf001.region where r_regionkey>2
       );
     
